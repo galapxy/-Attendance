54e76366419f3dd22f2f7aeeaad80450d25489d2
Ryota Honda <galapxy@outlook.jp>
2018-06-20 21:31:27 +0000

    current user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,18 @@
-require 'test_helper'
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+     log_in user
+     redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
 
-class SessionsControllerTest < ActionDispatch::IntegrationTest
-
-  test "should get new" do
-    get login_path
-    assert_response :success
+  def destroy
   end
 end
