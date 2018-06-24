9d5e7afa4be9b6e65cf18a828c484d0dd7def61e
Ryota Honda <galapxy@outlook.jp>
2018-06-24 14:01:18 +0000

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,6 +40,17 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
   
+    # アカウントを有効にする rist11.35
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # 有効化用のメールを送信する rist11.35
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+  
   private
 
     # メールアドレスをすべて小文字にする
