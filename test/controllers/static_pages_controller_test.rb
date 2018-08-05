b4a5005106751d91874376c77d8f8af0a203f5a5
Ryota Honda <galapxy@outlook.jp>
2018-08-05 06:30:24 +0000

    add blog

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -29,4 +29,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
+  
+  test "should get blog" do
+    get blog_path
+    assert_response :success
+    assert_select "title", "Blog | #{@base_title}"
+  end
 end
